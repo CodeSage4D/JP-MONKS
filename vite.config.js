ee900a0e70e534640d2073a75f491b31927743fc
JP Monks Team <solutions@jpmonks.com>
2026-05-19 23:40:11 +0530

    chore: add Vite server and dev-compiler setup

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'esbuild', // Built-in high-performance minification
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/js/[name].[hash].js',
+        chunkFileNames: 'assets/js/[name].[hash].js',
+        assetFileNames: 'assets/[ext]/[name].[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+});
